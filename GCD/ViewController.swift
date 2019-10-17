32bbefc2d0d1f0edeab0733a5c1b3c4357809d6e
nikaperepelkina <nikakoda@yandex.ru>
2019-10-17 16:03:23 +0700

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by Ника Перепелкина on 17.10.2019.
+//  Copyright © 2019 Nika Perepelkina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
